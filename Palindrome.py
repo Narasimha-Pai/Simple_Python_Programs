48e19235bc3ebd081fb592d7f36c225c38a99dc0
S Narasimha Pai <113236680+Narasimha-Pai@users.noreply.github.com>
2023-09-21 23:46:23 +0530

    Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,5 +1,5 @@
 word = input("Enter the Word")
-rev = word[ : : -1]      #[ : : -1] means form infinte to infinite in reverse order
+rev = word[ : : -1]      #[ : : -1] means from infinte to infinite in reverse order
 if word == rev:
     print(word, "is a palindrome")
 else:
